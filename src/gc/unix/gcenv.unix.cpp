0d53e1411cbea53adb252e854cd9a6c60e0101d2
Guillaume B <gboucher90@users.noreply.github.com>
2017-07-21 14:08:11 +0200

    Add CGroup CFS CPU limit support (#12797)

--- src/gc/unix/gcenv.unix.cpp
+++ src/gc/unix/gcenv.unix.cpp
@@ -69,6 +69,7 @@ static pthread_mutex_t g_flushProcessWriteBuffersMutex;
 
 size_t GetRestrictedPhysicalMemoryLimit();
 bool GetWorkingSetSize(size_t* val);
+bool GetCpuLimit(uint32_t* val);
 
 static size_t g_RestrictedPhysicalMemoryLimit = 0;
 
@@ -493,6 +494,7 @@ bool GCToOSInterface::GetCurrentProcessAffinityMask(uintptr_t* processAffinityMa
 uint32_t GCToOSInterface::GetCurrentProcessCpuCount()
 {
     uintptr_t pmask, smask;
+    uint32_t cpuLimit;
 
     if (!GetCurrentProcessAffinityMask(&pmask, &smask))
         return 1;
@@ -515,6 +517,9 @@ uint32_t GCToOSInterface::GetCurrentProcessCpuCount()
     // maximum of 64 here.
     if (count == 0 || count > 64)
         count = 64;
+
+    if (GetCpuLimit(&cpuLimit) && cpuLimit < count)
+        count = cpuLimit;
 
     return count;
 }
